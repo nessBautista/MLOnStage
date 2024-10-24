106e7e37b4173ef99f2973583907c9051876e22d
Ness <nestor.jbautista@gmail.com>
2024-10-24 08:44:25 -0600

    aur-171-WIP

--- data_pipeline/elt_script/elt_script.py
+++ data_pipeline/elt_script/elt_script.py
@@ -1,64 +1,68 @@
 import subprocess
 import time
 
+
 def wait_for_postgres(host, max_retries=5, delay_seconds=5):
+    """Wait for PostgreSQL to become available."""
     retries = 0
     while retries < max_retries:
         try:
             result = subprocess.run(
-                ["pg_isready", "-h", host], check=True, capture_output=True, text=True
-            )
+                ["pg_isready", "-h", host], check=True, capture_output=True, text=True)
             if "accepting connections" in result.stdout:
-                print("Successfully connected to Postgres")
+                print("Successfully connected to PostgreSQL!")
                 return True
         except subprocess.CalledProcessError as e:
-            print(f"Failed to connect to Postgres: {e}")
+            print(f"Error connecting to PostgreSQL: {e}")
             retries += 1
-            print(f"Retrying in {delay_seconds} seconds...")
+            print(
+                f"Retrying in {delay_seconds} seconds... (Attempt {retries}/{max_retries})")
             time.sleep(delay_seconds)
     print("Max retries reached. Exiting.")
     return False
 
 
+# Use the function before running the ELT process
 if not wait_for_postgres(host="source_postgres"):
     exit(1)
 
-print("Starting ELT Script...")
+print("Starting ELT script...")
+
+# Configuration for the source PostgreSQL database
 source_config = {
     'dbname': 'source_db',
     'user': 'postgres',
     'password': 'secret',
+    # Use the service name from docker-compose as the hostname
     'host': 'source_postgres'
 }
 
+# Configuration for the destination PostgreSQL database
 destination_config = {
     'dbname': 'destination_db',
     'user': 'postgres',
     'password': 'secret',
+    # Use the service name from docker-compose as the hostname
     'host': 'destination_postgres'
 }
 
+# Use pg_dump to dump the source database to a SQL file
 dump_command = [
     'pg_dump',
     '-h', source_config['host'],
     '-U', source_config['user'],
     '-d', source_config['dbname'],
     '-f', 'data_dump.sql',
-    '-w' # Do not prompt for password
+    '-w'  # Do not prompt for password
 ]
 
-# set the PGPassword environment variable to avoid password prompt
+# Set the PGPASSWORD environment variable to avoid password prompt
 subprocess_env = dict(PGPASSWORD=source_config['password'])
 
-# execute the dump command
-subprocess.run(
-    dump_command,     # List containing pg_dump command and its arguments
-    env=subprocess_env,  # Environment variables, including PGPASSWORD
-    check=True        # Raise CalledProcessError if the command fails
-)
+# Execute the dump command
+subprocess.run(dump_command, env=subprocess_env, check=True)
 
-
-# use psql to load the dumped SQL file into the destination database
+# Use psql to load the dumped SQL file into the destination database
 load_command = [
     'psql',
     '-h', destination_config['host'],
@@ -67,15 +71,10 @@ load_command = [
     '-a', '-f', 'data_dump.sql'
 ]
 
-# set the PGPassword environment variable for the destination database
+# Set the PGPASSWORD environment variable for the destination database
 subprocess_env = dict(PGPASSWORD=destination_config['password'])
 
 # Execute the load command
-subprocess.run(
-    load_command,   # List containing psql command and its arguments
-    env=subprocess_env, # Environment variables, including PGPASSWORD
-    check=True         # Raise CalledProcessError if the command fails
-    
-)
+subprocess.run(load_command, env=subprocess_env, check=True)
 
-print("ELT Script completed successfully.")
+print("Ending ELT script...")
